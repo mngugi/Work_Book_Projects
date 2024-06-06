d7f26be1880028f365f66bd291652d0aa30fccd4
M.ngugi <mngugi7@gmail.com>
2024-06-06 16:05:15 +0300

    Create NumericalSimulations.py

---
+++ NumericalSimulations.py
@@ -0,0 +1,13 @@
+# a python model to compute
+# height of a ball in vertical motions
+
+# Formula : y = v0 t  - 0.5gt2
+
+v0 = 5
+g = 9.81
+t = 0.6
+
+
+y = v0 * t  - 0.5 *g * t ** 2
+
+print("Vertical position is :",y)
