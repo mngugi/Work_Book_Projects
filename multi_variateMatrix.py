04ca4ac057c9e3d33367135b6170e3d50244c388
M.ngugi <mngugi7@gmail.com>
2024-07-31 11:17:00 +0300

    Update multi_variateMatrix.py

--- multi_variateMatrix.py
+++ multi_variateMatrix.py
@@ -7,3 +7,22 @@ def matrix_multivariet(num,matrix):
         main.append(temp)
     return main
         
+def transpose(matrix):
+    main - list()
+    for i in range(len(matrix[0])):
+        temp = list()
+        for j in range (len(matrix)):
+            temp.append(matrix[j][i])
+        main.append(temp)
+    return main
+
+def vaector_multivariate(matrix1,matrix2):
+    main = list()
+    for i in matrix1:
+        temp_list = list()
+        for j in transpose(matrix2):
+            tempt = 0
+            for q in range(len(i)):
+                temp += i[q]*j[q]
+            temp_list.append(temp)    
+        
