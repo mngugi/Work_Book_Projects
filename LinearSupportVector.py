d6f93a3a8425023f38028bc55d6c53e81021f87b
M.ngugi <mngugi7@gmail.com>
2024-08-28 13:28:06 +0300

    Create LinearSupportVector.py

---
+++ LinearSupportVector.py
@@ -0,0 +1,35 @@
+# implementation of linear support vector SVM
+
+# import all the required packages
+
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+import matplotlib.pyplot as plt
+
+# Load a simple dataset (e.g., iris dataset)
+iris = datasets.load_iris()
+X = iris.data[:, :2]  # Taking only the first two features for visualization
+y = (iris.target != 0) * 1  # Binary classification (versus non-setosa)
+
+# Split the dataset into training and testing
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Create and train the SVM model
+svm_model = SVC(kernel='linear', C=1)
+svm_model.fit(X_train, y_train)
+
+# Plot decision boundary
+plt.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap='winter')
+ax = plt.gca()
+xlim = ax.get_xlim()
+ylim = ax.get_ylim()
+
+xx = np.linspace(xlim[0], xlim[1], 30)
+yy = np.linspace(ylim[0], ylim[1], 30)
+YY, XX = np.meshgrid(yy, xx)
+xy = np.vstack([XX.ravel(), YY.ravel()]).T
+Z = svm_model.decision_function(xy).reshape(XX.shape)
+
+ax.contour(XX, YY, Z, colors='k', levels=[-1, 0, 1], alpha=0.5, linestyles=['--', '-', '--'])
+plt.show()
