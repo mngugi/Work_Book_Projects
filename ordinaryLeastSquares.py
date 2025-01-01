243a56d315ea0d16bc0778ef796717278248b139
M.Ngugi <mngugi7@gmail.com>
2025-01-01 22:15:10 +0300

    Update ordinaryLeastSquares.py

--- ordinaryLeastSquares.py
+++ ordinaryLeastSquares.py
@@ -40,6 +40,20 @@ print("Parameters: ", res.params)
 print("Standard errors: ", res.bse)
 print("Predicted values: ", res.predict())
 
+pred_ols = res.get_prediction()
+iv_l = pred_ols.summary_frame()["obs_ci_lower"]
+iv_u = pred_ols.summary_frame()["obs_ci_upper"]
+
+fig, ax = plt.subplots(figsize=(8, 6))
+
+ax.plot(x, y, "o", label="data")
+ax.plot(x, y_true, "b-", label="True")
+ax.plot(x, res.fittedvalues, "r--.", label="OLS")
+ax.plot(x, iv_u, "r--")
+ax.plot(x, iv_l, "r--")
+ax.legend(loc="best")
+
+
 
 
 
