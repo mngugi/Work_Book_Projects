66ccea7c0100d99034c4a911ed7e63d724532acc
M.Ngugi <mngugi7@gmail.com>
2025-01-01 22:14:56 +0300

    Update ordinaryLeastSquares.py

--- ordinaryLeastSquares.py
+++ ordinaryLeastSquares.py
@@ -24,4 +24,22 @@ print(results.summary())
 print("Parameters: ", results.params)
 print("R2: ", results.rsquared)
 
+nsample = 50
+sig = 0.5
+x = np.linspace(0, 20, nsample)
+X = np.column_stack((x, np.sin(x), (x - 5) ** 2, np.ones(nsample)))
+beta = [0.5, 0.5, -0.02, 5.0]
+
+y_true = np.dot(X, beta)
+y = y_true + sig * np.random.normal(size=nsample)
+
+res = sm.OLS(y, X).fit()
+print(res.summary())
+
+print("Parameters: ", res.params)
+print("Standard errors: ", res.bse)
+print("Predicted values: ", res.predict())
+
+
+
 
