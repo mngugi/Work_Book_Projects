5b6dbd8aee86ff60cdb54245715472a29ea1c8b2
M.Ngugi <mngugi7@gmail.com>
2025-01-02 23:18:35 +0300

    Update multicollinearityOLS.py

--- multicollinearityOLS.py
+++ multicollinearityOLS.py
@@ -1,5 +1,6 @@
 from statsmodels.datasets.longley import load_pandas
 import statsmodels.api as sm
+import numpy as np   
 
 y = load_pandas().endog
 X = load_pandas().exog
@@ -20,4 +21,19 @@ norm_xtx = np.dot(norm_x.T, norm_x)
 eigs = np.linalg.eigvals(norm_xtx)
 condition_number = np.sqrt(eigs.max() / eigs.min())
 print(condition_number)
+
+ols_results2 = sm.OLS(y.iloc[:14], X.iloc[:14]).fit()
+print(
+    "Percentage change %4.2f%%\n"
+    * 7
+    % tuple(
+        [
+            i
+            for i in (ols_results2.params - ols_results.params)
+            / ols_results.params
+            * 100
+        ]
+    )
+)
+
 
