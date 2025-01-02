075ea8ecd21e3c052498ce308f4a961abe8e5a52
M.Ngugi <mngugi7@gmail.com>
2025-01-02 23:17:05 +0300

    Update multicollinearityOLS.py

--- multicollinearityOLS.py
+++ multicollinearityOLS.py
@@ -8,3 +8,16 @@ X = sm.add_constant(X)
 ols_model = sm.OLS(y, X)
 ols_results = ols_model.fit()
 print(ols_results.summary())
+
+# One way to assess multicollinearity is to compute the condition number. 
+norm_x = X.values
+for i, name in enumerate(X):
+    if name == "const":
+        continue
+    norm_x[:, i] = X[name] / np.linalg.norm(X[name])
+norm_xtx = np.dot(norm_x.T, norm_x)
+
+eigs = np.linalg.eigvals(norm_xtx)
+condition_number = np.sqrt(eigs.max() / eigs.min())
+print(condition_number)
+
